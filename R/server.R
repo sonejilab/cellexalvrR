bde36e7a4622dda4d6bf1dcb5f5790dcb1d94ea3
Stefan Lang <stefan.lang@med.lu.se>
2020-09-16 10:12:37 +0200

    Server function now adds screenshots to the log file.

--- R/server.R
+++ R/server.R
@@ -3,6 +3,11 @@
 #' Instead of loading and saving all files for each function call in the VR process
 #' we now can have one R process that sequencially works on all requests.
 #'
+#' The server will look for a file called <file>.input.R amnd process all commands in the file 
+#' using source(fname) so please be careful there!
+#'
+#' While the file <file>.input.lock is existing the input.R file is processed by the R and should not be touched.
+#' 
 #' @name server
 #' @aliases server,character-method
 #' @rdname server-methods
@@ -11,31 +16,38 @@
 #' The function sets a paste(file, 'input.lock', sep='.') lock file on processed scripts.
 #' To shut down the server process you can either write a q('no') into the script file or remove the pid file.
 #' @param file the file core name to create input.R input.log and pid files.
-#' @param sleepT sleep time in seconds between checks for the input.R file
+#' @param sleepT sleep time in seconds between checks for the input.R file (default 1)
 #' @param debug create the server output file? default FALSE
 #' @param masterPID if this pid is not active any more stop the server (default NULL)
+#' @param asFunction do not shut down R when exiting (default FALSE)
 #' @keywords server
 #' @title start a server function periodicly sourcing in a file.
 #' @export server
 
 if ( ! isGeneric('server') ){setGeneric('server', ## Name
-			function ( file, sleepT=1, debug=FALSE, masterPID = NULL ) { 
+			function ( file, sleepT=1, debug=FALSE, masterPID = NULL, asFunction =FALSE ) { 
 				standardGeneric('server') 
 			}
 	) }
 
 setMethod('server', signature = c ('character'),
-		definition =  function(file, sleepT=1, debug=FALSE, masterPID = NULL){
+		definition =  function(file, sleepT=1, debug=FALSE, masterPID = NULL, asFunction =FALSE ){
 	lockfile   = paste( file, 'input.lock', sep=".") 
 	scriptfile = paste( file, 'input.R', sep="." )
 	pidfile    = paste( file, 'pid', sep='.')
 
-	t = lapply( c( lockfile, scriptfile, pidfile) , function(file) { if(file.exists(file)) { unlink(file)} })
+	workSource = paste( file, 'input.working', sep="." )
+
+	t = lapply( c( lockfile, scriptfile, pidfile) ,
+	  function(file) {
+	    if(file.exists(file)) { unlink(file) } 
+	  }
+	)
 
 	cat( Sys.getpid() , file = pidfile )
 	
 	outFile = file(paste( file,  Sys.getpid(), 'output', sep=".") )
-
+	
 	if (! is.null(masterPID)) {
 		masterPID = ps::ps_handle( pid = as.integer(masterPID) )
 	}
@@ -44,15 +56,34 @@ setMethod('server', signature = c ('character'),
 		pv_file    = paste( file, 'cellexalvrR.version', sep='.')
 		file.create(pv_file)
 		cat( as.character(packageVersion("cellexalvrR")), file= pv_file, append=F)
+		
 		## redirect appendll output to output file
-		sink(outFile)
+		## Error is captured by the VR application and this is important to leave it like that.
+		if ( ! asFunction ) { sink(outFile) }
 	}
+
+	cellexalObj@outpath =  dirname(file)
+	
 	message ( paste( "server is starting - reading from file:\n", scriptfile))
 	message ( paste( "server debug mode:", debug))
 	if ( !is.null(masterPID) ) {
 		message ( paste( "server is checking the process",masterPID,"and shuts down if it is shutted down"))
 	}
+
+	## we check for additional screenshots in folder
+	path = file.path(dirname(file), 'Screenshots')
+	oldFiles = newScreenshots( c(), path =path )
+
   	while(TRUE){
+  		newFiles = newScreenshots(oldFiles, path=path)
+  		if ( length(newFiles) > 0 ){
+
+			for (n in newFiles) {
+				message( paste( 'logFigure will log the file',file.path(path, n) ))
+				cellexalObj = logFigure( cellexalObj,png= file.path(path, n), text='New VR screenshot.' )
+			}
+			oldFiles = c( oldFiles, newFiles)
+		}
 		if ( ! file.exists(pidfile ) ) {
 			break
 		}
@@ -62,11 +93,14 @@ setMethod('server', signature = c ('character'),
                 }
 				file.create(lockfile)
 				if ( debug ) {
-               	 cat ( readLines( scriptfile), file= outFile, sep="\n\r", append=TRUE )
+					cmd = readLines( scriptfile)
+					cmd = stringr::str_replace_all( paste( collapse=" " ,cmd), '\\s+', ' ')
+               	    cat ( c("", cmd,""), file= outFile, sep="\n\r", append=TRUE )
+               	    message( paste("VR cmd:", cmd) ) ## will go into the R_log.txt
             	}
                 try ( {source( scriptfile ) } )
-                file.remove( scriptfile )
-				file.remove(lockfile)
+                file.remove(scriptfile)
+                file.remove(lockfile)
         }
         if ( ! is.null( masterPID ) ){
         	#
@@ -82,6 +116,9 @@ setMethod('server', signature = c ('character'),
 		if ( ! is.null(cellexalObj@usedObj$sessionName ) ) {
 			message( "closing session" );
 			cellexalObj = renderReport( cellexalObj )
+			if ( debug ) {
+				cat("cellexalObj = renderReport( cellexalObj )")
+			}
 		}
 		message( "saving the main object" );
 		#lockedSave( cellexalObj ) ##the renderReport does that
@@ -91,7 +128,33 @@ setMethod('server', signature = c ('character'),
 		sink()
 		close(outFile)
 	}
-	q('no')
+	if ( ! asFunction ) {
+		q('no')
+	}
+	
 }
 )
 
+
+
+
+#' @name newScreenshots
+#' @aliases newScreenshots,character-method
+#' @rdname newScreenshots-methods
+#' @docType methods
+#' @description Check the folder '../Screenshots' for files
+#' @param oldFiles a vector of old files.
+#' @param path the path to look for file (default '../Screenshots' )
+#' @keywords server
+#' @title internal function!
+#' @export 
+newScreenshots <- function(oldFiles, path ='../Screenshots/' ) {
+		files = list.files( path )
+		OK = NULL
+		for ( x in files ){
+			if ( all ( sapply( oldFiles, function(old) { ! old==x } )) ){
+				OK = c(OK, x)
+			}
+		}
+		OK
+	}
